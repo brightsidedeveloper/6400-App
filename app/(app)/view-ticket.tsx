8a69ab455ba0715c6abb2c73a0af0ac43b2f4c48
Tim <timvan0118@gmail.com>
2024-04-14 20:17:58 -0500

    anotha one

---
+++ app/(app)/view-ticket.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+import React from "react"
+
+export default function ViewTicket() {
+  return (
+    <View>
+      <Text>view-ticket</Text>
+    </View>
+  )
+}
